a6dda24703024f205cd44bbbf6af9c7c21cd84bb
Nat <pangpondwee@gmail.com>
2023-11-27 17:00:18 +0700

    Modify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,9 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware() {}, {
   callbacks: {
     authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname === "/home" || req.nextUrl.pathname === "/") {
-        return token?.role === "admin" || token?.role === "user";
-      } else if (req.nextUrl.pathname === "/account-management") {
+      if (req.nextUrl.pathname === "/account-management") {
         return token?.role === "admin";
       }
-
       return Boolean(token);
     },
   },
