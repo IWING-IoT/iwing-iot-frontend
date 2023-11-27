dec8d9567e00acf89951b34fb53b1c9f25631f27
Nat <pangpondwee@gmail.com>
2023-11-27 17:18:52 +0700

    fix: loading indicator

--- src/components/atoms/loading-indicator.tsx
+++ src/components/atoms/loading-indicator.tsx
@@ -1,6 +1,6 @@
 export const LoadingIndicator = () => {
   return (
-    <div className="flex h-full items-center justify-center space-x-2 dark:invert">
+    <div className="flex h-full items-center justify-center space-x-2">
       <span className="sr-only">Loading...</span>
       <div className="h-4 w-4 animate-bounce rounded-full bg-primary [animation-delay:-0.3s]" />
       <div className="h-4 w-4 animate-bounce rounded-full bg-primary [animation-delay:-0.15s]" />
