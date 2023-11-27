da27816d39177ee0a65c6bbdf620ec5eb773f136
Nat <pangpondwee@gmail.com>
2023-11-27 17:01:26 +0700

    Add loading indicator

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { LoadingIndicator } from "@/components/atoms/loading-indicator";
+
+export default function Loading() {
+  return (
+    <div className="h-screen w-full">
+      <LoadingIndicator />
+    </div>
+  );
+}
---
+++ src/components/atoms/loading-indicator.tsx
@@ -0,0 +1,10 @@
+export const LoadingIndicator = () => {
+  return (
+    <div className="flex h-full items-center justify-center space-x-2 dark:invert">
+      <span className="sr-only">Loading...</span>
+      <div className="h-4 w-4 animate-bounce rounded-full bg-primary [animation-delay:-0.3s]" />
+      <div className="h-4 w-4 animate-bounce rounded-full bg-primary [animation-delay:-0.15s]" />
+      <div className="h-4 w-4 animate-bounce rounded-full bg-primary" />
+    </div>
+  );
+};
