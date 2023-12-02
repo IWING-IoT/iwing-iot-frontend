0705c6fd5cc78e099fe8d44576102c2b688376e5
Nat <pangpondwee@gmail.com>
2023-12-03 02:41:52 +0700

    feat: subpage of project

---
+++ src/app/(platform)/(project)/[projectId]/api-settings/page.tsx
@@ -0,0 +1,5 @@
+function ApiSettings() {
+  return <div>ApiSettings</div>;
+}
+
+export default ApiSettings;
--- src/app/(platform)/(project)/[projectId]/layout.tsx
+++ src/app/(platform)/(project)/[projectId]/layout.tsx
@@ -12,22 +12,31 @@ import {
   HeaderTitle,
   HeaderTitleAndSupporting,
 } from "@/components/molecules/header";
+import { NavTabs } from "@/components/molecules/nav-tabs";
 import MainContainer from "@/components/templates/main-container";
 import { Button } from "@/components/ui/button";
 import { fetchData } from "@/lib/data-fetching";
 import { TProjectDetails } from "@/lib/type";
 
+const tabs = [
+  { label: "Dashboard", href: "dashboard" },
+  { label: "Map", href: "map" },
+  { label: "Devices", href: "devices" },
+  { label: "API settings", href: "api-settings" },
+];
+
 type LayoutProps = {
   params: { projectId: string };
   children: React.ReactNode;
 };
+
 export default async function Layout({ params, children }: LayoutProps) {
   const { data }: { data: TProjectDetails } = await fetchData(
     `/project/${params.projectId}`,
   );
   return (
     <>
-      <Header>
+      <Header className="pb-0 sm:pb-0">
         {/* <Breadcrumb>
             <BreadcrumbItem>
               <BreadcrumbLink href="/home?sortBy=ascending">Home</BreadcrumbLink>
@@ -45,6 +54,7 @@ export default async function Layout({ params, children }: LayoutProps) {
             <ProjectMoreDropdown />
           </HeaderActions>
         </HeaderContent>
+        <NavTabs tabs={tabs} />
       </Header>
       <MainContainer>{children}</MainContainer>
     </>
