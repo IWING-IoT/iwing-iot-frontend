e79c5407bd73e12704b7b2c6b9854ab56386afb9
Nat <pangpondwee@gmail.com>
2023-12-05 20:13:03 +0700

    feat: logo svg component

--- src/components/atoms/logo.tsx
+++ src/components/atoms/logo.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import { LogoIcon } from "./logo-icon";
 
 export function Logo() {
   return (
     <Link href={"/home"} className="flex items-center gap-3">
-      <Image src={"/logo.svg"} width={32} height={32} alt="logo" />
+      <LogoIcon className="text-primary" width={32} height={32} />
       <span className="text-lg font-bold">IWING IoT</span>
     </Link>
   );
