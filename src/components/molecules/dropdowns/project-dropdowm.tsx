71eef82e6ea6e3c92f9e995c545bf13da9886c7f
Nat <pangpondwee@gmail.com>
2023-12-11 01:32:42 +0700

    refactor

--- src/components/molecules/dropdowns/project-dropdowm.tsx
+++ src/components/molecules/dropdowns/project-dropdowm.tsx
@@ -1,9 +1,11 @@
-import {
-  ProjectInfoDialogContent,
-  ProjectInfoDialogTrigger,
-} from "@/components/organisms/dialogs/project-info-dialog";
 import { Button } from "@/components/ui/button";
-import { Dialog } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,7 +14,17 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { TProjectDetails } from "@/lib/type";
-import { Archive, ChevronDown, Pen, Trash2 } from "lucide-react";
+import { formatDate } from "@/lib/utils";
+import {
+  Archive,
+  Calendar,
+  ChevronDown,
+  Info,
+  MapPin,
+  Pen,
+  Trash2,
+  User,
+} from "lucide-react";
 
 type ProjectMoreDropdownProps = {
   projectId: string;
@@ -55,5 +67,53 @@ export async function ProjectDropdown({
       </DropdownMenu>
       <ProjectInfoDialogContent projectData={projectData} />
     </Dialog>
+  );
+}
+
+type ProjectInfoDialogProps = {
+  projectData: TProjectDetails;
+};
+
+// Separate the trigger and the content of the dialog because of radix ui's bug
+
+export function ProjectInfoDialogTrigger() {
+  return (
+    <DialogTrigger asChild>
+      <DropdownMenuItem>
+        <Info className="h-4 w-4 text-muted-foreground" />
+        Project info
+      </DropdownMenuItem>
+    </DialogTrigger>
+  );
+}
+
+export function ProjectInfoDialogContent({
+  projectData,
+}: ProjectInfoDialogProps) {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{projectData.name}</DialogTitle>
+      </DialogHeader>
+      <div className="flex flex-col gap-4">
+        <p className="text-muted-foreground">
+          {projectData.description ?? "No description"}
+        </p>
+        <div className="flex flex-col gap-2 text-muted-foreground">
+          <div className="flex items-center gap-2">
+            <User className="h-5 w-5" />
+            <p>{projectData.ownerName}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <MapPin className="h-5 w-5" />
+            <p>{projectData.location.en_name}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <Calendar className="h-5 w-5" />
+            <p>{formatDate(projectData.startedAt)}</p>
+          </div>
+        </div>
+      </div>
+    </DialogContent>
   );
 }
