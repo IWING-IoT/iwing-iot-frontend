680f245a0793e22ab20043c590e86576d73a22df
Nat <pangpondwee@gmail.com>
2023-12-12 02:55:12 +0700

    feat: archive, delete project

--- src/components/molecules/dropdowns/project-dropdowm.tsx
+++ src/components/molecules/dropdowns/project-dropdowm.tsx
@@ -1,11 +1,8 @@
+"use client";
+import { DeleteActionDialog } from "@/components/organisms/dialogs/delete-action-dialog";
+import { DialogWithContent } from "@/components/organisms/dialogs/dialog-with-content";
+import { PatchActionDialog } from "@/components/organisms/dialogs/patch-action-dialog";
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -31,89 +28,101 @@ type ProjectMoreDropdownProps = {
   projectData: TProjectDetails;
 };
 
-export async function ProjectDropdown({
+export function ProjectDropdown({
   projectId,
   projectData,
 }: ProjectMoreDropdownProps) {
+  // deal with radix ui's bug
+  const onOpenChange = (open: boolean) => {
+    if (open === false) {
+      setTimeout(() => {
+        const escEvent = new KeyboardEvent("keydown", {
+          key: "Escape",
+        });
+        document.dispatchEvent(escEvent);
+      }, 200);
+    }
+  };
+  const onSelect = (e: Event) => {
+    e.preventDefault();
+  };
   return (
-    <Dialog>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant={"ghost"}
-            size={"icon"}
-            className="h-8 w-8 sm:h-10 sm:w-10"
-          >
-            <ChevronDown className="h-5 w-5 sm:h-6 sm:w-6" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-60" align="end">
-          {/* Project info modal trigger */}
-          <ProjectInfoDialogTrigger />
-          <DropdownMenuItem>
-            <Pen className="h-4 w-4 text-muted-foreground" />
-            Edit
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          className="h-8 w-8 sm:h-10 sm:w-10"
+        >
+          <ChevronDown className="h-5 w-5 sm:h-6 sm:w-6" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-60" align="end">
+        <DialogWithContent
+          title={projectData.name}
+          content={
+            <div className="flex flex-col gap-4">
+              <p className="text-muted-foreground">
+                {projectData.description ?? "No description"}
+              </p>
+              <div className="flex flex-col gap-2 text-muted-foreground">
+                <div className="flex items-center gap-2">
+                  <User className="h-5 w-5" />
+                  <p>{projectData.ownerName}</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <MapPin className="h-5 w-5" />
+                  <p>{projectData.location.en_name}</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Calendar className="h-5 w-5" />
+                  <p>{formatDate(projectData.startedAt)}</p>
+                </div>
+              </div>
+            </div>
+          }
+          onOpenChange={onOpenChange}
+        >
+          <DropdownMenuItem onSelect={onSelect}>
+            <Info className="h-4 w-4 text-muted-foreground" />
+            Project info
           </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
+        </DialogWithContent>
+        <DropdownMenuItem>
+          <Pen className="h-4 w-4 text-muted-foreground" />
+          Edit
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <PatchActionDialog
+          variant="warning"
+          icon={<Archive />}
+          title="Archive this project"
+          description="You will no longer be able to make any changes to this project."
+          action="archiveProject"
+          id={projectId}
+          onOpenChange={onOpenChange}
+        >
+          <DropdownMenuItem onSelect={onSelect}>
             <Archive className="h-4 w-4 text-muted-foreground" />
             Archive
           </DropdownMenuItem>
-          <DropdownMenuItem className="text-destructive data-[highlighted]:text-destructive">
+        </PatchActionDialog>
+        <DeleteActionDialog
+          title="Delete this project"
+          description="Are you sure you want to delete this project? This action is irreversible and will result in permanent loss of all associated data."
+          action="deleteProject"
+          id={projectId}
+          onOpenChange={onOpenChange}
+        >
+          <DropdownMenuItem
+            className="text-destructive data-[highlighted]:text-destructive"
+            onSelect={onSelect}
+          >
             <Trash2 className="h-4 w-4" />
             Delete
           </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <ProjectInfoDialogContent projectData={projectData} />
-    </Dialog>
-  );
-}
-
-type ProjectInfoDialogProps = {
-  projectData: TProjectDetails;
-};
-
-// Separate the trigger and the content of the dialog because of radix ui's bug
-
-export function ProjectInfoDialogTrigger() {
-  return (
-    <DialogTrigger asChild>
-      <DropdownMenuItem>
-        <Info className="h-4 w-4 text-muted-foreground" />
-        Project info
-      </DropdownMenuItem>
-    </DialogTrigger>
-  );
-}
-
-export function ProjectInfoDialogContent({
-  projectData,
-}: ProjectInfoDialogProps) {
-  return (
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>{projectData.name}</DialogTitle>
-      </DialogHeader>
-      <div className="flex flex-col gap-4">
-        <p className="text-muted-foreground">
-          {projectData.description ?? "No description"}
-        </p>
-        <div className="flex flex-col gap-2 text-muted-foreground">
-          <div className="flex items-center gap-2">
-            <User className="h-5 w-5" />
-            <p>{projectData.ownerName}</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <MapPin className="h-5 w-5" />
-            <p>{projectData.location.en_name}</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <Calendar className="h-5 w-5" />
-            <p>{formatDate(projectData.startedAt)}</p>
-          </div>
-        </div>
-      </div>
-    </DialogContent>
+        </DeleteActionDialog>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
