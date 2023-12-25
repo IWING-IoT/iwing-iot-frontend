6b5d64be454ae7ee20db5c33ff2c98480dfe7763
Nat <pangpondwee@gmail.com>
2023-12-25 19:51:50 +0700

    style: use thai system font and adjust line-height

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ module.exports = {
       base: ["1rem", "1.5rem"],
       lg: ["1.125rem", "1.75rem"],
       xl: ["1.25rem", "1.875rem"],
-      "2xl": ["1.5rem", "2rem"],
-      "3xl": ["1.875rem", "2.375rem"],
+      "2xl": ["1.5rem", "2.4rem"],
+      "3xl": ["1.875rem", "3rem"],
       "4xl": ["2.25rem", { lineHeight: "2.75rem", letterSpacing: "-0.045rem" }],
       "5xl": ["3rem", { lineHeight: "3.75rem", letterSpacing: "-0.06rem" }],
       "6xl": ["3.75rem", { lineHeight: "4.5rem", letterSpacing: "-0.075rem" }],
@@ -96,11 +96,7 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       fontFamily: {
-        sans: [
-          "var(--font-inter)",
-          "var(--font-anuphan)",
-          ...defaultTheme.fontFamily.sans,
-        ],
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
         mono: ["var(--font-jetbrains-mono)", ...defaultTheme.fontFamily.mono],
       },
       height: {
