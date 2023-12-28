a6d49e01686ab8fee81d132b5c15ad42bb481ff9
Nat <pangpondwee@gmail.com>
2023-12-28 22:26:17 +0700

    feat: add restricted wrapper to api-settings-columns

--- src/components/columns/api-settings-columns.tsx
+++ src/components/columns/api-settings-columns.tsx
@@ -8,6 +8,7 @@ import { Button } from "../ui/button";
 import { DialogWithContent } from "../organisms/dialogs/dialog-with-content";
 import { EditApiFieldForm } from "../forms/edit-api-field-form";
 import { DeleteActionDialog } from "../organisms/dialogs/delete-action-dialog";
+import Restricted from "../providers/permission-provider/restricted";
 
 export const apiSettingsColumns: ColumnDef<TDeploymentApi>[] = [
   {
@@ -51,26 +52,28 @@ export const apiSettingsColumns: ColumnDef<TDeploymentApi>[] = [
     cell: ({ row }) => {
       const data = row.original;
       return (
-        <div className="flex justify-end gap-1">
-          <DialogWithContent
-            title={`Edit ${data.name}`}
-            content={<EditApiFieldForm fieldData={data} />}
-          >
-            <Button type="button" variant={"ghost"} size={"icon"}>
-              <Pen className="h-5 w-5 text-muted-foreground" />
-            </Button>
-          </DialogWithContent>
-          <DeleteActionDialog
-            title={`Delete ${data.name}`}
-            description="This action can't be undone. All data associated with this field will be lost."
-            action="deleteApiField"
-            id={data.id}
-          >
-            <Button type="button" variant={"ghost"} size={"icon"}>
-              <Trash2 className="h-5 w-5 text-destructive" />
-            </Button>
-          </DeleteActionDialog>
-        </div>
+        <Restricted to="edit">
+          <div className="flex justify-end gap-1">
+            <DialogWithContent
+              title={`Edit ${data.name}`}
+              content={<EditApiFieldForm fieldData={data} />}
+            >
+              <Button type="button" variant={"ghost"} size={"icon"}>
+                <Pen className="h-5 w-5 text-muted-foreground" />
+              </Button>
+            </DialogWithContent>
+            <DeleteActionDialog
+              title={`Delete ${data.name}`}
+              description="This action can't be undone. All data associated with this field will be lost."
+              action="deleteApiField"
+              id={data.id}
+            >
+              <Button type="button" variant={"ghost"} size={"icon"}>
+                <Trash2 className="h-5 w-5 text-destructive" />
+              </Button>
+            </DeleteActionDialog>
+          </div>
+        </Restricted>
       );
     },
   },
