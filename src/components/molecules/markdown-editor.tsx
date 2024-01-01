77afee4ccf48520c90b8f398502a954ba8bdfc1e
Nat <pangpondwee@gmail.com>
2024-01-02 01:09:39 +0700

    improve responsive

--- src/components/molecules/markdown-editor.tsx
+++ src/components/molecules/markdown-editor.tsx
@@ -1,5 +1,8 @@
+"use client";
+import useMediaQuery from "beautiful-react-hooks/useMediaQuery";
 import { CodeEditor } from "../atoms/code-editor";
 import { MarkdownPreview } from "../atoms/markdown-preview";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 
 type MarkDownEditorProps = {
   value: string;
@@ -7,6 +10,23 @@ type MarkDownEditorProps = {
 };
 
 export function MarkDownEditor({ value, setValue }: MarkDownEditorProps) {
+  const isMobile = useMediaQuery("(max-width: 640px)");
+  if (isMobile) {
+    return (
+      <Tabs defaultValue="editor">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="editor">Editor</TabsTrigger>
+          <TabsTrigger value="preview">Preview</TabsTrigger>
+        </TabsList>
+        <TabsContent value="editor">
+          <CodeEditor value={value} setValue={setValue} />
+        </TabsContent>
+        <TabsContent value="preview">
+          <MarkdownPreview value={value} />
+        </TabsContent>
+      </Tabs>
+    );
+  }
   return (
     <div className="relative flex flex-1 gap-6 [&>*]:flex-1">
       <CodeEditor value={value} setValue={setValue} />
