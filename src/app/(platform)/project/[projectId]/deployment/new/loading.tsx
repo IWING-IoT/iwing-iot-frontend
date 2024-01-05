aa896ec5349cf4b8f9669acfdf79fd01a36342af
Nat <pangpondwee@gmail.com>
2024-01-05 22:02:00 +0700

    remove create deployment page

--- src/app/(platform)/project/[projectId]/deployment/new/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingIndicator } from "@/components/atoms/loading-indicator";
-
-export default function Loading() {
-  return (
-    <div className="h-screen w-full">
-      <LoadingIndicator />
-    </div>
-  );
-}
