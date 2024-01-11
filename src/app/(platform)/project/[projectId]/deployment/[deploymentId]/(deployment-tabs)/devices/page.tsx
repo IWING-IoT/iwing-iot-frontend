4db7152094db358f3188f8554e21f8bed85123da
Nat <pangpondwee@gmail.com>
2024-01-11 15:51:31 +0700

    feat: init deployment devices

--- src/app/(platform)/project/[projectId]/deployment/[deploymentId]/(deployment-tabs)/devices/page.tsx
+++ src/app/(platform)/project/[projectId]/deployment/[deploymentId]/(deployment-tabs)/devices/page.tsx
@@ -1,12 +1,24 @@
 import { deploymentDevicesColumns } from "@/components/columns/deployment-devices-columns";
 import { DataTable } from "@/components/data-table/data-table";
+import {
+  CardHeader,
+  CardHeaderActions,
+  CardHeaderDescription,
+  CardHeaderTextContent,
+  CardHeaderTitle,
+} from "@/components/molecules/card-header";
+import { MainContainer } from "@/components/templates/main-container";
 import { TableWrapper } from "@/components/templates/table-wrapper";
+import { Button } from "@/components/ui/button";
 import { fetchData } from "@/lib/data-fetching";
 // import { deploymentDevicesData } from "@/lib/mock";
 import { TDeploymentDeviceDetails } from "@/lib/type";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 type DeploymentDevicesProps = {
   params: {
+    projectId: string;
     deploymentId: string;
   };
 };
@@ -17,13 +29,36 @@ export default async function DeploymentDevices({
   const { data: deploymentDevicesData }: { data: TDeploymentDeviceDetails[] } =
     await fetchData(`/phase/${params.deploymentId}/device`);
   return (
-    <TableWrapper>
-      <DataTable
-        columns={deploymentDevicesColumns}
-        data={deploymentDevicesData}
-        searchByColumn="alias"
-        enableToggleColumns
-      />
-    </TableWrapper>
+    <MainContainer>
+      <TableWrapper>
+        <CardHeader>
+          <CardHeaderTextContent>
+            <CardHeaderTitle>Devices</CardHeaderTitle>
+            <CardHeaderDescription>
+              View and manage devices in this deployment.
+            </CardHeaderDescription>
+          </CardHeaderTextContent>
+          <CardHeaderActions>
+            <Button asChild>
+              <Link
+                href={`/project/${params.projectId}/deployment/${params.deploymentId}/device/add`}
+              >
+                <Plus className="mr-2 h-5 w-5" />
+                Add device
+              </Link>
+            </Button>
+          </CardHeaderActions>
+        </CardHeader>
+        <DataTable
+          columns={deploymentDevicesColumns}
+          data={deploymentDevicesData}
+          searchByColumn="alias"
+          enableToggleColumns
+          clickableRows
+          clickableRowsBaseURL={`/project/${params.projectId}/deployment/${params.deploymentId}/device`}
+          clickableRowsTrailURL="/overview"
+        />
+      </TableWrapper>
+    </MainContainer>
   );
 }
