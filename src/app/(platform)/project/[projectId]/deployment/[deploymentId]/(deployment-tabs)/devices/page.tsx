540dbcea39618542c886ca6f93ce43918d477f8d
Nat <pangpondwee@gmail.com>
2024-01-16 20:55:56 +0700

    feat: add restricted component

--- src/app/(platform)/project/[projectId]/deployment/[deploymentId]/(deployment-tabs)/devices/page.tsx
+++ src/app/(platform)/project/[projectId]/deployment/[deploymentId]/(deployment-tabs)/devices/page.tsx
@@ -7,11 +7,11 @@ import {
   CardHeaderTextContent,
   CardHeaderTitle,
 } from "@/components/molecules/card-header";
+import Restricted from "@/components/providers/permission-provider/restricted";
 import { MainContainer } from "@/components/templates/main-container";
 import { TableWrapper } from "@/components/templates/table-wrapper";
 import { Button } from "@/components/ui/button";
 import { fetchData } from "@/lib/data-fetching";
-// import { deploymentDevicesData } from "@/lib/mock";
 import { TDeploymentDeviceDetails } from "@/lib/type";
 import { Plus } from "lucide-react";
 import Link from "next/link";
@@ -38,16 +38,18 @@ export default async function DeploymentDevices({
               View and manage devices in this deployment.
             </CardHeaderDescription>
           </CardHeaderTextContent>
-          <CardHeaderActions>
-            <Button asChild>
-              <Link
-                href={`/project/${params.projectId}/deployment/${params.deploymentId}/device/add`}
-              >
-                <Plus className="mr-2 h-5 w-5" />
-                Add device
-              </Link>
-            </Button>
-          </CardHeaderActions>
+          <Restricted to="edit">
+            <CardHeaderActions>
+              <Button asChild>
+                <Link
+                  href={`/project/${params.projectId}/deployment/${params.deploymentId}/device/add`}
+                >
+                  <Plus className="mr-2 h-5 w-5" />
+                  Add device
+                </Link>
+              </Button>
+            </CardHeaderActions>
+          </Restricted>
         </CardHeader>
         <DataTable
           columns={deploymentDevicesColumns}
