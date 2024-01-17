98bbbfae1de737c04feebb02e3f11011b31b7b9c
Nat <pangpondwee@gmail.com>
2024-01-17 15:27:13 +0700

    feat: remove firmware

--- src/app/(platform)/project/[projectId]/deployment/[deploymentId]/device/[deviceId]/(device-tabs)/firmware/page.tsx
+++ src/app/(platform)/project/[projectId]/deployment/[deploymentId]/device/[deviceId]/(device-tabs)/firmware/page.tsx
@@ -1,12 +1,13 @@
 import { DeviceFirmwareForm } from "@/components/forms/device-firmware-form";
 import { FirmwareCard } from "@/components/molecules/device-firmware-card";
+import { DeleteActionDialog } from "@/components/organisms/dialogs/delete-action-dialog";
 import Restricted from "@/components/providers/permission-provider/restricted";
 import { CardGrid } from "@/components/templates/card-grid";
 import { Button } from "@/components/ui/button";
 import { fetchData } from "@/lib/data-fetching";
 import { TDeviceFirmware, TFirmware, TFirmwareType } from "@/lib/type";
 import { firmwareType } from "@/lib/utils";
-import { FilePlus, Pen } from "lucide-react";
+import { FilePlus, Pen, Trash2 } from "lucide-react";
 import dynamic from "next/dynamic";
 
 const DialogWithContent = dynamic(
@@ -53,18 +54,45 @@ export default async function Firmware({ params }: FirmwareProps) {
               type={type}
               actionType="edit"
               firmwareName={deviceFirmwareData?.[type]?.firmware.name ?? ""}
+              firmwareId={deviceFirmwareData?.[type]?.firmware.id ?? ""}
               versionName={
                 deviceFirmwareData?.[type]?.firmwareVersion.name ?? ""
               }
+              versionId={deviceFirmwareData?.[type]?.firmwareVersion.id ?? ""}
               autoUpdate={deviceFirmwareData?.[type]?.autoUpdate ?? false}
               button={
                 <Restricted to="edit">
-                  <DialogWithContent title={`Edit`} content={<div>Hello</div>}>
-                    <Button variant={"outline"} className="flex-1">
-                      <Pen className="mr-2 h-5 w-5" />
-                      Edit
-                    </Button>
-                  </DialogWithContent>
+                  <div className="flex flex-1 gap-3">
+                    <DialogWithContent
+                      title={`Edit ${firmwareType[type].toLowerCase()}`}
+                      content={
+                        <DeviceFirmwareForm
+                          action="edit"
+                          deviceFirmwareId={
+                            deviceFirmwareData[type]?.id as string
+                          }
+                          type={type}
+                          firmwareData={firmwareData[type]}
+                          deviceFirmwareData={deviceFirmwareData[type]}
+                        />
+                      }
+                    >
+                      <Button variant={"outline"} className="flex-1">
+                        <Pen className="mr-2 h-5 w-5" />
+                        Edit
+                      </Button>
+                    </DialogWithContent>
+                    <DeleteActionDialog
+                      title={`Remove ${firmwareType[type].toLowerCase()}`}
+                      description="We'll no longer keep track of this firmware. You can assign them again later."
+                      action="removeDeviceFirmware"
+                      id={deviceFirmwareData[type]?.id as string}
+                    >
+                      <Button variant={"outline"} size={"icon"}>
+                        <Trash2 className="h-5 w-5 text-destructive" />
+                      </Button>
+                    </DeleteActionDialog>
+                  </div>
                 </Restricted>
               }
             />
@@ -78,11 +106,12 @@ export default async function Firmware({ params }: FirmwareProps) {
               button={
                 <Restricted to="edit">
                   <DialogWithContent
-                    title={`Assign ${firmwareType[type]}`}
+                    title={`Assign ${firmwareType[type].toLowerCase()}`}
                     content={
                       <DeviceFirmwareForm
                         action="assign"
                         type={type}
+                        deviceId={params.deviceId}
                         firmwareData={firmwareData[type]}
                       />
                     }
--- src/components/forms/device-firmware-form.tsx
+++ src/components/forms/device-firmware-form.tsx
@@ -1,44 +1,73 @@
 "use client";
-import { TDeviceFirmwareDetails, TFirmware, TFirmwareType } from "@/lib/type";
+import {
+  TDeviceFirmwareDetails,
+  TFirmware,
+  TFirmwareDetails,
+  TFirmwareType,
+  TFirmwareVersion,
+  THttpError,
+} from "@/lib/type";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "../ui/form";
-import { useQuery } from "@tanstack/react-query";
-import { fetchData } from "@/lib/data-fetching";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  clientFetchData,
+  fetchData,
+  patchData,
+  postData,
+} from "@/lib/data-fetching";
 import { Button } from "../ui/button";
 import { Combobox } from "../organisms/combobox";
 import { cn, firmwareType, generateEscEvent } from "@/lib/utils";
 import { ChevronsUpDown } from "lucide-react";
 import { DialogFooter } from "../ui/dialog";
+import { RadioGroup } from "../ui/radio-group";
+import {
+  CustomRadioItem,
+  CustomRadioItemContainer,
+  CustomRadioItemLabel,
+} from "../molecules/custom-radio-item";
+import { Checkbox } from "../ui/checkbox";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 type DeviceFirmwareFormProps =
   | {
       action: "assign";
       type: TFirmwareType;
+      deviceId: string;
+      deviceFirmwareId?: never;
       deviceFirmwareData?: never;
       firmwareData: TFirmware[];
     }
   | {
       action: "edit";
       type: TFirmwareType;
-      deviceFirmwareData: TDeviceFirmwareDetails;
+      deviceId?: never;
+      deviceFirmwareId: string;
+      deviceFirmwareData: TDeviceFirmwareDetails | undefined;
       firmwareData: TFirmware[];
     };
 
 export function DeviceFirmwareForm({
   action,
   type,
+  deviceId,
+  deviceFirmwareId,
   deviceFirmwareData,
   firmwareData,
 }: DeviceFirmwareFormProps) {
+  const router = useRouter();
   const firmwareList: { label: string; value: string }[] = firmwareData.map(
     (firmware) => ({
       label: firmware.name,
@@ -53,21 +82,75 @@ export function DeviceFirmwareForm({
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      firmware: action === "edit" ? deviceFirmwareData.firmware.id : "",
-      version: action === "edit" ? deviceFirmwareData.firmwareVersion.id : "",
-      autoUpdate: action === "edit" ? deviceFirmwareData.autoUpdate : true,
+      firmware: action === "edit" ? deviceFirmwareData?.firmware.id : "",
+      version: action === "edit" ? deviceFirmwareData?.firmwareVersion.id : "",
+      autoUpdate: action === "edit" ? deviceFirmwareData?.autoUpdate : false,
     },
     mode: "onChange",
   });
   const firmware = form.watch("firmware");
-  const { data: firmwareVersionData } = useQuery({
-    queryKey: [firmware],
-    queryFn: () => fetchData(`/firmware/${firmware}`),
+  const { data: versionList } = useQuery({
+    queryKey: ["firmware", firmware],
+    queryFn: async () => {
+      const {
+        data: firmwareVersionData,
+      }: { data: TFirmwareDetails | undefined } = await clientFetchData(
+        `/firmware/${firmware}`,
+      );
+      if (firmwareVersionData) {
+        const versionList = firmwareVersionData.versions.map((version) => ({
+          label: version.name,
+          value: version.id,
+        }));
+        return versionList;
+      }
+      return [];
+    },
     enabled: Boolean(firmware),
   });
+  const assignFirmware = useMutation({
+    mutationFn: (data: {
+      type: TFirmwareType;
+      id: string;
+      autoUpdate: boolean;
+    }) => postData(`/devicePhase/${deviceId}/firmware`, data),
+    onError: (error: THttpError) => {
+      toast.error("Unable to assign firmware", {
+        description: error.response.data.message,
+      });
+    },
+    onSuccess: () => {
+      toast.success("Firmware assigned successfully");
+      router.refresh();
+    },
+  });
+  const editFirmware = useMutation({
+    mutationFn: (data: { id: string; autoUpdate: boolean }) =>
+      patchData(`/deviceFirmware/${deviceFirmwareId}`, data),
+    onError: (error: THttpError) => {
+      toast.error("Unable to save changes", {
+        description: error.response.data.message,
+      });
+    },
+    onSuccess: () => {
+      toast.success("Changes saved successfully");
+      router.refresh();
+    },
+  });
   function onSubmit(data: z.infer<typeof formSchema>) {
-    console.log("submit");
     console.log(data);
+    if (action === "assign") {
+      assignFirmware.mutate({
+        type,
+        id: data.version,
+        autoUpdate: data.autoUpdate,
+      });
+    } else if (action === "edit") {
+      editFirmware.mutate({
+        id: data.version,
+        autoUpdate: data.autoUpdate,
+      });
+    }
     generateEscEvent();
   }
   return (
@@ -92,6 +175,7 @@ export function DeviceFirmwareForm({
                 emptyPlaceholder={`No ${firmwareType[
                   type
                 ].toLowerCase()} found`}
+                drawerHeader={`Select ${firmwareType[type].toLowerCase()}`}
               >
                 <FormControl>
                   <Button
@@ -112,6 +196,75 @@ export function DeviceFirmwareForm({
                 </FormControl>
               </Combobox>
               <FormMessage />
+            </FormItem>
+          )}
+        />
+        {versionList ? (
+          <FormField
+            control={form.control}
+            name="version"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Version</FormLabel>
+                <Combobox
+                  options={versionList}
+                  value={field.value}
+                  onSelect={(value) => form.setValue("version", value)}
+                  searchPlaceholder={`Search by version name`}
+                  emptyPlaceholder={`No versions found`}
+                  drawerHeader={`Select version`}
+                >
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className={cn(
+                        "justify-between",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value
+                        ? versionList.find(
+                            (version) => version.value === field.value,
+                          )?.label ?? "Select version"
+                        : "Select version"}
+                      <ChevronsUpDown className="h-5 w-5" />
+                    </Button>
+                  </FormControl>
+                </Combobox>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        ) : (
+          <div className="flex flex-col gap-2">
+            <p className="text-sm font-medium leading-none text-muted-foreground">
+              Version
+            </p>
+            <Button variant="outline" className="justify-between" disabled>
+              Select version
+              <ChevronsUpDown className="h-5 w-5" />
+            </Button>
+          </div>
+        )}
+        <FormField
+          control={form.control}
+          name="autoUpdate"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow-xs">
+              <FormControl>
+                <Checkbox
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <div className="space-y-1 leading-none">
+                <FormLabel>Enable auto-update</FormLabel>
+                <FormDescription>
+                  When there's a newer version of this firmware, it will be
+                  updated automatically.
+                </FormDescription>
+              </div>
             </FormItem>
           )}
         />
