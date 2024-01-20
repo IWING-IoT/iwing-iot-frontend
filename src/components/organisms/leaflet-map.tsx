58643dedeaf32d2a87bde1941f9b8386926a7c51
Nat <pangpondwee@gmail.com>
2024-01-20 21:03:22 +0700

    feat: edit leaflet map

--- src/components/organisms/leaflet-map.tsx
+++ src/components/organisms/leaflet-map.tsx
@@ -1,42 +1,154 @@
 "use client";
 
-import { MapContainer, Marker, TileLayer, Tooltip } from "react-leaflet";
+import {
+  LayerGroup,
+  LayersControl,
+  MapContainer,
+  Marker,
+  Polygon,
+  Polyline,
+  TileLayer,
+  Tooltip,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import { cn } from "@/lib/utils";
+import { useAtom } from "jotai";
+import { mapActionAtom } from "@/store/atoms";
+import { useEffect } from "react";
 
-type LeafletMapProps = {
-  center: [number, number];
-  zoom: number;
-  scrollWheelZoom: boolean;
-  className?: string;
-  markers?: { position: [number, number]; content: React.ReactNode }[];
-};
+type LeafletMapProps =
+  | {
+      type: "default";
+      scrollWheelZoom: boolean;
+      className?: string;
+      markers: {
+        id: string;
+        position: [number, number];
+        content: React.ReactNode;
+      }[];
+      layers?: never;
+    }
+  | {
+      type: "withLayerControl";
+      scrollWheelZoom: boolean;
+      className?: string;
+      markers?: never;
+      layers: {
+        name: string;
+        checked: boolean;
+        markers?: {
+          id: string;
+          position: [number, number];
+          content: React.ReactNode;
+        }[];
+        vectors?: {
+          id: string;
+          position: [number, number][];
+          color: string;
+          type: "polyline" | "polygon";
+          content: React.ReactNode;
+        }[];
+      }[];
+    };
 
 export default function LeafletMap({
-  center,
-  zoom,
+  type,
   scrollWheelZoom,
   className,
   markers,
+  layers,
 }: LeafletMapProps) {
   return (
     <MapContainer
-      className={cn("z-0 h-full min-h-[20rem] w-full", className)}
-      center={center}
-      zoom={zoom}
+      className={cn("z-0 h-full min-h-[32rem] w-full", className)}
+      bounds={
+        type === "default"
+          ? markers?.map((marker) => marker.position)
+          : layers
+              .map((layer) =>
+                layer.markers
+                  ? layer.markers?.map((marker) => marker.position)
+                  : layer.vectors?.map((vector) => vector.position),
+              )
+              .flat()
+              .filter((item): item is [number, number] => item !== undefined)
+      }
       scrollWheelZoom={scrollWheelZoom}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {markers?.map((marker, index) => (
-        <Marker key={index} position={marker.position}>
-          <Tooltip>{marker.content}</Tooltip>
-        </Marker>
-      ))}
+      {type === "default" ? (
+        <>
+          {markers?.map((marker, index) => (
+            <Marker key={`${marker.id}-${index}`} position={marker.position}>
+              <Tooltip>{marker.content}</Tooltip>
+            </Marker>
+          ))}
+        </>
+      ) : (
+        <LayersControl position="topleft">
+          {layers.map((layer, index) => (
+            <LayersControl.Overlay
+              key={`${layer.name}-${index}`}
+              checked={layer.checked}
+              name={layer.name}
+            >
+              <LayerGroup>
+                {layer.markers?.map((marker, index) => (
+                  <Marker
+                    key={`${marker.id}-${index}`}
+                    position={marker.position}
+                  >
+                    <Tooltip>{marker.content}</Tooltip>
+                  </Marker>
+                ))}
+                {layer.vectors?.map((vector, index) =>
+                  vector.type === "polygon" ? (
+                    <Polygon
+                      key={`${vector.id}-${index}`}
+                      pathOptions={{ color: vector.color }}
+                      positions={vector.position}
+                    >
+                      <Tooltip>{vector.content}</Tooltip>
+                    </Polygon>
+                  ) : (
+                    <Polyline
+                      key={`${vector.id}-${index}`}
+                      pathOptions={{ color: vector.color }}
+                      positions={vector.position}
+                    >
+                      <Tooltip>{vector.content}</Tooltip>
+                    </Polyline>
+                  ),
+                )}
+              </LayerGroup>
+            </LayersControl.Overlay>
+          ))}
+        </LayersControl>
+      )}
+      <MapAction />
     </MapContainer>
   );
+}
+
+function MapAction() {
+  const [mapAction, setMapAction] = useAtom(mapActionAtom);
+  const map = useMap();
+  useEffect(() => {
+    if (mapAction !== null) {
+      if (mapAction.type === "flyTo") {
+        map.flyTo(mapAction.position, 10);
+      } else if (mapAction.type === "flyToBounds") {
+        map.flyToBounds(mapAction.position);
+      }
+      setMapAction(null);
+    }
+  }, [mapAction, map, setMapAction]);
+  return null;
 }
