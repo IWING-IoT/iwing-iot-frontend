7da4b48700356e327011e4b61a7363523c6dfe2c
Nat <pangpondwee@gmail.com>
2024-01-27 15:12:15 +0700

    feat: leaflet draw

--- src/components/molecules/leaflet-draw-control.tsx
+++ src/components/molecules/leaflet-draw-control.tsx
@@ -8,9 +8,10 @@ import { useAtom } from "jotai";
 import { geoJsonAtom, showDialogAtom } from "@/store/atoms";
 import { MapMarkerAreaForm } from "../forms/map-marker-area-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { postData } from "@/lib/data-fetching";
+import { deleteData, patchData, postData, putData } from "@/lib/data-fetching";
 import { TArea, TCustomMarker, THttpError } from "@/lib/type";
 import { toast } from "sonner";
+import { useDebouncedCallback } from "use-debounce";
 
 interface Props {
   type: "polygon" | "marker";
@@ -59,6 +60,30 @@ export function LeafletDrawControl({ type, deploymentId }: Props) {
       queryClient.invalidateQueries({ queryKey: ["customMarker"] });
     },
   });
+  const editMarker = useMutation({
+    mutationFn: (data: TCustomMarker) => patchData(`/mark/${data.id}`, data),
+    onError: (error: THttpError) => {
+      toast.error("Unable to save changes", {
+        description: error.response.data.message,
+      });
+    },
+    onSuccess: () => {
+      toast.success("Changes saved successfully");
+      queryClient.invalidateQueries({ queryKey: ["customMarker"] });
+    },
+  });
+  const deleteMarker = useMutation({
+    mutationFn: (markerId: string) => deleteData(`/mark/${markerId}`),
+    onError: (error: THttpError) => {
+      toast.error("Unable to delete marker", {
+        description: error.response.data.message,
+      });
+    },
+    onSuccess: () => {
+      toast.success("Marker deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["customMarker"] });
+    },
+  });
   const addArea = useMutation({
     mutationFn: (data: Pick<TArea, "name" | "description" | "coordinates">) =>
       postData(`/phase/${deploymentId}/area`, data),
@@ -72,12 +97,37 @@ export function LeafletDrawControl({ type, deploymentId }: Props) {
       queryClient.invalidateQueries({ queryKey: ["area"] });
     },
   });
+  const editArea = useMutation({
+    mutationFn: (data: Omit<TArea, "alert">) =>
+      putData(`/area/${data.id}`, data),
+    onError: (error: THttpError) => {
+      toast.error("Unable to save changes", {
+        description: error.response.data.message,
+      });
+    },
+    onSuccess: () => {
+      toast.success("Changes saved successfully");
+      queryClient.invalidateQueries({ queryKey: ["area"] });
+    },
+  });
+  const deleteArea = useMutation({
+    mutationFn: (areaId: string) => deleteData(`/area/${areaId}`),
+    onError: (error: THttpError) => {
+      toast.error("Unable to delete area", {
+        description: error.response.data.message,
+      });
+    },
+    onSuccess: () => {
+      toast.success("Area deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["area"] });
+    },
+  });
 
   const handleCreate = (e: any) => {
-    console.log(e.layer.toGeoJSON().geometry.type);
+    // console.log(e.layer.toGeoJSON().geometry.type);
     setShowDialog({
       show: true,
-      title: "New",
+      title: `Add new ${type === "marker" ? "marker" : "area"}`,
       children: (
         <MapMarkerAreaForm
           handleSubmit={(data) => {
@@ -105,30 +155,71 @@ export function LeafletDrawControl({ type, deploymentId }: Props) {
       ),
     });
   };
-
-  const handleEdit = () => {
+  const handleEdit = (e: any) => {
     const geo = ref.current?.toGeoJSON();
-    console.log(geo);
     if (geo?.type === "FeatureCollection") {
-      setGeoJson(geo);
+      // setGeoJson(geo);
+      // console.log(geo.features.find((item) => item.properties.id === "1"));
+      if (type === "polygon") {
+        e.layers.getLayers().forEach((layer: any) => {
+          // console.log(layer);
+          const editedLayer = geo.features.find(
+            (item) => item.properties.id === layer.feature.properties.id,
+          );
+          const editedArea = {
+            id: editedLayer?.properties.id,
+            name: editedLayer?.properties.name,
+            description: editedLayer?.properties.description,
+            coordinates: (editedLayer?.geometry as any).coordinates[0].map(
+              (item: any) => item.slice().reverse(),
+            ),
+            isActive: false,
+          };
+          // console.log(editedArea);
+          editArea.mutate(editedArea);
+        });
+      } else {
+        e.layers.getLayers().forEach((layer: any) => {
+          const editedLayer = geo.features.find(
+            (item) => item.properties.id === layer.feature.properties.id,
+          );
+          const editedMarker = {
+            id: editedLayer?.properties.id,
+            name: editedLayer?.properties.name,
+            description: editedLayer?.properties.description,
+            latitude: (editedLayer?.geometry as any).coordinates[1],
+            longitude: (editedLayer?.geometry as any).coordinates[0],
+          };
+          editMarker.mutate(editedMarker);
+        });
+      }
     }
   };
-
-  const handleDelete = () => {
-    const geo = ref.current?.toGeoJSON();
-    console.log(geo);
-    if (geo?.type === "FeatureCollection") {
-      setGeoJson(geo);
+  const debouncedHandleEdit = useDebouncedCallback(handleEdit, 500);
+  const handleDelete = (e: any) => {
+    console.log("delete event triggered");
+    console.log(e.layers.getLayers());
+    if (type === "polygon") {
+      // console.log(e.layers.getLayers());
+      e.layers.getLayers().forEach((layer: any) => {
+        deleteArea.mutate(layer.feature.properties.id);
+      });
+    } else {
+      e.layers.getLayers().forEach((layer: any) => {
+        deleteMarker.mutate(layer.feature.properties.id);
+        // ref.current?.removeLayer(layer);
+      });
     }
   };
+  const debouncedHandleDelete = useDebouncedCallback(handleDelete, 500);
 
   return (
     <FeatureGroup ref={ref}>
       <EditControl
         position="topleft"
-        onEdited={handleEdit}
+        onEdited={(e) => debouncedHandleEdit(e)}
         onCreated={handleCreate}
-        onDeleted={handleDelete}
+        onDeleted={(e) => debouncedHandleDelete(e)}
         draw={{
           rectangle: false,
           circle: false,
