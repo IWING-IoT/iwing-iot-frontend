b71fe87e85cbdff75f4cbcabb3a2164da55d416e
Nat <pangpondwee@gmail.com>
2024-01-27 23:20:25 +0700

    feat: chart card

--- src/components/atoms/chart.tsx
+++ src/components/atoms/chart.tsx
@@ -9,82 +9,54 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-import { Card, CardHeader, CardTitle } from "../ui/card";
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 type ChartProps = {
-  type: "tinyLineChart" | "simpleLineChart";
+  type: "simpleLineChart";
+  xAxisDataKey: string;
+  yAxisDataKey: string;
+  yDomain?: [number, number];
+  data: { [x: string]: number | string }[];
 };
 
-export function Chart({ type }: ChartProps) {
-  if (type === "tinyLineChart") {
-    return (
-      <ResponsiveContainer width="100%" height="100%" minHeight={80}>
-        <LineChart width={300} height={100} data={data}>
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
-        </LineChart>
-      </ResponsiveContainer>
-    );
-  }
+export function Chart({
+  type,
+  xAxisDataKey,
+  yAxisDataKey,
+  yDomain,
+  data,
+}: ChartProps) {
+  // if (type === "tinyLineChart") {
+  //   return (
+  //     <ResponsiveContainer width="100%" height="100%" minHeight={80}>
+  //       <LineChart width={300} height={100} data={data}>
+  //         <Line
+  //           type="monotone"
+  //           dataKey="pv"
+  //           stroke="hsl(var(--primary))"
+  //           strokeWidth={2}
+  //         />
+  //       </LineChart>
+  //     </ResponsiveContainer>
+  //   );
+  // }
   return (
-    <ResponsiveContainer width="100%" height="100%">
+    <ResponsiveContainer width="100%" height="100%" minHeight={240}>
       <LineChart
         width={730}
         height={250}
         data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        margin={{ top: 0, right: 16, left: 0, bottom: 0 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
+        <XAxis dataKey={xAxisDataKey} />
+        <YAxis domain={yDomain} />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        <Line
+          type="monotone"
+          dataKey={yAxisDataKey}
+          stroke="hsl(var(--primary))"
+        />
       </LineChart>
     </ResponsiveContainer>
   );
