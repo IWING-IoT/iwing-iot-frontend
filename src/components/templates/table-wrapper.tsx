80ee66f0ec4f4e4aed798603f797a9a7198b768d
Nat <pangpondwee@gmail.com>
2024-01-29 19:34:37 +0700

    feat: accept className

--- src/components/templates/table-wrapper.tsx
+++ src/components/templates/table-wrapper.tsx
@@ -1,10 +1,15 @@
+import { cn } from "@/lib/utils";
+
 type TableWrapperProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
-export function TableWrapper({ children }: TableWrapperProps) {
+export function TableWrapper({ children, className }: TableWrapperProps) {
   return (
-    <div className="overflow-hidden rounded-xl border shadow-xs">
+    <div
+      className={cn("overflow-hidden rounded-xl border shadow-xs", className)}
+    >
       {children}
     </div>
   );
